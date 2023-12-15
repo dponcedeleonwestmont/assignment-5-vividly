d154c4d550204a4de5b7ebeb1c92da10c03339d8
David <dponce@LAPTOP-38R23ENN>
2023-12-14 18:51:20 -0800

    s

--- src/classifier/valorant_classifier/pyspark_runner.py
+++ src/classifier/valorant_classifier/pyspark_runner.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("Valorant").getOrCreate()
-data = spark.read.csv("player_stats.csv", header=True, inferSchema=True)
+data = spark.read.csv("../..player_stats2.csv", header=True, inferSchema=True)
 
 processed_data = data.filter(data['kill'] > 10).select('number', 'player')
 
